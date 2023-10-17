e7107c11c0f1deb4febe9d529cf68a02233621ad
Eduardo Fernandes <90912186+EduFrnds@users.noreply.github.com>
2023-10-17 16:42:45 -0300

    API-001
    - Criação do Exception
    - Alteração admin.py e validators.

--- library/admin.py
+++ library/admin.py
@@ -3,35 +3,40 @@ from django.contrib import admin
 from library.book_collection.models import BookCollection
 from library.book_renter.models import BookRenter
 from library.books.models import Book
-# Register your models here.
 from library.collection.models import Collection
 
 
 class Books(admin.ModelAdmin):
-    list_display = ('id', 'title', 'authors', 'translators',
-                    'edition','pages','publishing_company')
+    list_display = ('id', 'title', 'authors', 'translators', 'edition', 'pages', 'publishing_company')
     list_display_links = ('id', 'title')
     search_fields = ['title']
     list_per_page = 20
     ordering = ['title']
 
+
 admin.site.register(Book, Books)
+
 
 class Collections(admin.ModelAdmin):
     list_display = ('id', 'knowledge_area')
     list_display_link = ('id', 'knowledge_area')
     search_fields = ['knowledge_area']
 
+
 admin.site.register(Collection, Collections)
+
 
 class BookCollections(admin.ModelAdmin):
     list_display = ('id', 'title', 'knowledge_area', 'condition', 'knowledge_level')
     list_display_link = ['id']
 
+
 admin.site.register(BookCollection, BookCollections)
+
 
 class BookRenters(admin.ModelAdmin):
     list_display = ('id', 'name', 'address', 'phone_number', 'email')
     list_display_link = ['name']
+
 
 admin.site.register(BookRenter, BookRenters)
--- library/validators.py
+++ library/validators.py
@@ -1,14 +1,8 @@
 import re
 
-#Validate arquivo
 
 def phone_number_valid(phone_number):
     """check if the cell phone is valid(xx 9xxxx-xxxx)"""
     modelo = '[0-9]{2} [0-9]{5}-[0-9]{4}'
-    resposta = re.findall(modelo, phone_number)
-    return resposta
-
-#def name_valid(name):
-#    modelo_name = ''
-#    resposta_name = re.findall(modelo_name, name)
-#    return resposta_name
+    resposta_esperada = re.findall(modelo, phone_number)
+    return resposta_esperada
